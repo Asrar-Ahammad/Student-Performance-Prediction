4270ed6bcc8aba741a6f38cbc8f82b6610b2faa3
Asrar-Ahammad <asrarahammadshaik@gmail.com>
2023-11-23 14:17:03 +0530

    Final update streamlit

--- app.py
+++ app.py
@@ -1,67 +1,51 @@
-# Delete app.py before deployment on AWS.
-
-from flask import Flask, request, render_template
+import streamlit as st
 import numpy as np
 import pandas as pd
-
 from sklearn.preprocessing import StandardScaler
 
 from src.pipeline.predict_pipeline import Predictpipeline, CustomData
 
-application = Flask(__name__)
-
-app = application
-
 # Route to home page
-
-
-@app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/predictdata',methods=['GET','POST'])
-def predict_datapoint():
-    if request.method=='GET':
-            return render_template('home.html')
-    else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
-
-        predict_pipeline=Predictpipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        bestf = 'Keep up the good work'
-        abavgf = 'Good work'
-        avgf = 'You must work hard'
-        failed = 'You Failed'
-
-        print("after Prediction")
-        if results >= 100 or results >=90:
-            return render_template('home.html',results= 100, feed= bestf)
-        elif results <= 90 and results >=70:
-            return render_template('home.html',results= results[0], feed= abavgf)
-        elif results <= 70 and results >=35:
-            return render_template('home.html',results= results[0], feed= avgf)
-        elif results < 35:
-            return render_template('home.html',results= results[0], feed= failed)
-        else:
-             return render_template('home.html',results= results[0])
-        
+    st.title('Student Performance Prediction')
+    st.write('Please fill in the following details:')
+    gender = st.selectbox('Gender', ['male', 'female'])
+    race_ethnicity = st.selectbox('Ethnicity', ['group A', 'group B', 'group C', 'group D', 'group E'])
+    parental_education = st.selectbox('Parental Education', ['some high school', 'high school', 'some college', "associate's degree", "bachelor's degree", "master's degree"])
+    lunch = st.selectbox('Lunch Type', ['standard', 'free/reduced'])
+    test_prep = st.selectbox('Test Preparation Course', ['none', 'completed'])
+    reading_score = st.number_input('Reading Score', min_value=0, max_value=100)
+    writing_score = st.number_input('Writing Score', min_value=0, max_value=100)
     
+    if st.button('Predict'):
+        data = CustomData(
+            gender=gender,
+            race_ethnicity=race_ethnicity,
+            parental_level_of_education=parental_education,
+            lunch=lunch,
+            test_preparation_course=test_prep,
+            reading_score=float(reading_score),
+            writing_score=float(writing_score)
+        )
+        pred_df = data.get_data_as_data_frame()
+        
+        predict_pipeline = Predictpipeline()
+        results = predict_pipeline.predict(pred_df)
+        
+        if results >= 100 or results >= 90:
+            st.success('Results: 100')
+            st.write('Keep up the good work!')
+        elif results <= 90 and results >= 70:
+            st.success(f'Results: {results}')
+            st.write('Good work!')
+        elif results <= 70 and results >= 35:
+            st.warning(f'Results: {results}')
+            st.write('You must work hard.')
+        elif results < 35:
+            st.error(f'Results: {results}')
+            st.write('You failed.')
+        else:
+            st.write(f'Results: {results}')
 
-if __name__=='__main__':
-     app.run(debug=False, host='0.0.0.0',port=5000) 
-    #  app.run(debug=False, host='0.0.0.0',port=5001) 
-     
-# print('http://127.0.0.1:5001/predictdata')
+if __name__ == '__main__':
+    index()
